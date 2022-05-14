4454e82f169c25c8122d59f2b788e64152b22048
jhoh96 <dearlybeloved0214@gmail.com>
2022-05-14 13:04:35 +0900

    implemented chat

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const http = require('http');
+const WebSocket = require("ws");
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server:server, path:'/websocket' });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+/**
+ * Basic Sending/Receing Text Data
+ */
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//   });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+//   ws.send('hello');
+// });
+
+/**
+ * Broadcasting to all clients including itself
+ */
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
